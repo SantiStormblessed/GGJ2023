1cdfe40da1701f721c24c36124e536fb04144c88
deceptibot5a <deceptibot5a@gmail.com>
2023-02-04 14:49:53 -0500

    Platform prefabs

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,17 +5,8 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [SerializeField] private float life = 3f;
-    //private Transform playerTransform;
 
     void Awake() {
         Destroy(gameObject, life);
-        //playerTransform = FindObjectOfType<PlayerMovement>().GetPlayer();
     }
-
-    //se pueden borrar pero ajá XD
-    /*void OnCollisionEnter2D(Collision2D collision) {
-        if (collision.gameObject.CompareTag("Player")) { 
-        }
-    }*/
-    
 }
