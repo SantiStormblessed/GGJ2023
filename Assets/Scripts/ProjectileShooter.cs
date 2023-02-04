6e76cfb1beaf421431a6ef6341a062e921d51af0
deceptibot5a <deceptibot5a@gmail.com>
2023-02-04 18:58:40 -0500

    Proyectiles en proceso

--- Assets/Scripts/ProjectileShooter.cs
+++ Assets/Scripts/ProjectileShooter.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class ProjectileShooter : MonoBehaviour
 {
-    [SerializeField] private Transform projectileSpawnPoint;
-    [SerializeField] private GameObject projectilePrefab;
+    [SerializeField] private Transform[] projectileSpawnPoint;
+    [SerializeField] private GameObject[] projectilePrefab;
     [SerializeField] private float projectileSpeed = 10f;
+    private int random;
+    private int fairness;
+
+    void Start() {
+        //random = random.Next(6, 15);
+    }
 
     void Update() {
         if (Input.GetKeyDown(KeyCode.Space)) {
-            var projectile = Instantiate(projectilePrefab, projectileSpawnPoint.position, projectileSpawnPoint.rotation);
-            projectile.GetComponent<Rigidbody2D>().velocity = projectileSpawnPoint.right * -1f * projectileSpeed;
+            var projectile = Instantiate(projectilePrefab[0], projectileSpawnPoint[0].position, projectileSpawnPoint[0].rotation);
+            projectile.GetComponent<Rigidbody2D>().velocity = projectileSpawnPoint[0].right * -1f * projectileSpeed;
         }
     }
 }
