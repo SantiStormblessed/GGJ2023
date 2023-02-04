04bdea52817ea271d5b5a85811a8e3ea8e0e08e9
SantiStormblessed <santi.ssf@gmail.com>
2023-02-04 14:48:11 -0500

    Level generation progress

--- Assets/Scripts/Level Generation/PlatformGenController.cs
+++ Assets/Scripts/Level Generation/PlatformGenController.cs
@@ -5,15 +5,32 @@ using UnityEngine;
 public class PlatformGenController : MonoBehaviour
 {
     [SerializeField] GameObject player;
+    [SerializeField] GameObject platform;
+    [SerializeField] GameObject brokenPlatform;
+    [SerializeField] GameObject cloudPlatform;
+    [SerializeField] GameObject movingPlatform;
 
-    PlatformFall PlatformFall;
+    float currHeight = -3f;
+
     Rigidbody2D playerRB;
-    //float playerHeight = 0;
-
 
     void Start()
     {
-        playerRB = player.GetComponent<Rigidbody2D>();
+        while (currHeight <= 35f){
+            Instantiate(platform, new Vector3(Random.Range(-7.882f, -1.006f), currHeight, 0), Quaternion.identity);
+            currHeight += Random.Range(1f, 3.3f);
+        }
+
+        while (currHeight <= 75f){
+            Instantiate(platform, new Vector3(Random.Range(-7.882f, -1.006f), currHeight, 0), Quaternion.identity);
+            currHeight += Random.Range(1f, 3.3f);
+        }
+
+        while (currHeight <= 115f){
+            Instantiate(platform, new Vector3(Random.Range(-7.882f, -1.006f), currHeight, 0), Quaternion.identity);
+            currHeight += Random.Range(1f, 3.3f);
+        }
+        
     }
 
     // Update is called once per frame
