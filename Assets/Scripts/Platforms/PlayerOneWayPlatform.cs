b605e288f16e90c01bd2b6b5078f596e0fdd0dc2
deceptibot5a <deceptibot5a@gmail.com>
2023-02-05 13:21:20 -0500

    El rojo ya se puede mover con las flechas

--- Assets/Scripts/Platforms/PlayerOneWayPlatform.cs
+++ Assets/Scripts/Platforms/PlayerOneWayPlatform.cs
@@ -7,14 +7,6 @@ public class PlayerOneWayPlatform : MonoBehaviour
     private GameObject currentOneWayPlatform;
     [SerializeField] private BoxCollider2D playerCollider;
 
-    void Update() {
-        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) {
-            if (currentOneWayPlatform != null) {
-                StartCoroutine(DisableCollision());
-            }
-        }
-    }
-
     private void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.CompareTag("OneWayPlatform")) {
            currentOneWayPlatform = collision.gameObject;
@@ -24,11 +16,5 @@ public class PlayerOneWayPlatform : MonoBehaviour
         if (collision.gameObject.CompareTag("OneWayPlatform")) {
             currentOneWayPlatform = null;
         }
-    }
-    private IEnumerator DisableCollision() {
-        BoxCollider2D platformCollider = currentOneWayPlatform.GetComponent<BoxCollider2D>();
-        Physics2D.IgnoreCollision(playerCollider, platformCollider);
-        yield return new WaitForSeconds(0.25f);
-        Physics2D.IgnoreCollision(playerCollider, platformCollider, false);
     }
 }
