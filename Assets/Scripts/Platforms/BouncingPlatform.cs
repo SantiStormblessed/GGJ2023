326072ed64f414a73245081ede54c22925af6a35
deceptibot5a <deceptibot5a@gmail.com>
2023-02-05 13:35:53 -0500

    Ahí tienen su *** timer pintado

--- Assets/Scripts/Platforms/BouncingPlatform.cs
+++ Assets/Scripts/Platforms/BouncingPlatform.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class BouncingPlatform : MonoBehaviour
 {
+    private float cooldown = 0.5f;
     private float bounce = 20f;
-    
-    private void OnCollisionEnter2D(Collision2D collision) {
-        if((collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Player2")) && collision.gameObject.transform.position.y > transform.position.y) { 
-        collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounce, ForceMode2D.Impulse);
+
+    void Update() {
+        if (cooldown <= 0.5f) {
+            cooldown += Time.deltaTime;
         }
     }
-}
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if ((collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Player2")) && collision.gameObject.transform.position.y > transform.position.y) {
+            if (cooldown >= 0.5f) {
+                collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounce, ForceMode2D.Impulse);
+                cooldown = 0f;
+                }
+            }
+        }
+    }
