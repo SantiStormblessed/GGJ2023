a85e14747e98097ee48b986a8acae4fcc80612cb
SantiStormblessed <santi.ssf@gmail.com>
2023-02-04 23:51:32 -0500

    Platform sprites added, background, fixed platform interaction with player 2 and no se que mas todo bien la buena 👍🐦❤😍🤦‍♂️🤦‍♀️🎶😎🎉💋💋

--- Assets/Scripts/Platforms/BouncingPlatform.cs
+++ Assets/Scripts/Platforms/BouncingPlatform.cs
@@ -7,7 +7,7 @@ public class BouncingPlatform : MonoBehaviour
     private float bounce = 20f;
     
     private void OnCollisionEnter2D(Collision2D collision) {
-        if(collision.gameObject.CompareTag("Player")) { 
+        if(collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Player2")) { 
         collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounce, ForceMode2D.Impulse);
         }
     }
