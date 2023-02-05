d06a80a80ac330df0c1b627d26b1a4d3dddccb65
SantiStormblessed <santi.ssf@gmail.com>
2023-02-05 01:07:14 -0500

    Le cambié hasta los calzoncillos

---
+++ Assets/Scripts/SunRotation.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunRotation : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+         transform.Rotate(new Vector3(0,0,20) * Time.deltaTime);
+    }
+}
