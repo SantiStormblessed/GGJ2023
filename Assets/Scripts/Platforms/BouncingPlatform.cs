abbb04d0727e0d198bef197c4a1ae5e346a8d262
deceptibot5a <deceptibot5a@gmail.com>
2023-02-05 12:37:18 -0500

    Nube corregida

--- Assets/Scripts/Platforms/BouncingPlatform.cs
+++ Assets/Scripts/Platforms/BouncingPlatform.cs
@@ -7,7 +7,7 @@ public class BouncingPlatform : MonoBehaviour
     private float bounce = 20f;
     
     private void OnCollisionEnter2D(Collision2D collision) {
-        if(collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Player2")) { 
+        if((collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Player2")) && collision.gameObject.transform.position.y > transform.position.y) { 
         collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounce, ForceMode2D.Impulse);
         }
     }
