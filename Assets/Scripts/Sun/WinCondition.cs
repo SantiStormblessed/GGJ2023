cdd124a4422e814d101c3a324f2c7571dcbed362
deceptibot5a <deceptibot5a@gmail.com>
2023-02-05 09:27:39 -0500

    ya hay juego ggez

---
+++ Assets/Scripts/Sun/WinCondition.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinCondition : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.CompareTag("Player")) {
+            SceneManager.LoadScene("BlueVictory");
+        }
+        if (collision.gameObject.CompareTag("Player2")) {
+            SceneManager.LoadScene("RedVictory");
+        }
+    }
+}
